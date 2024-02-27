b0d90e9fca63953db0a8a299c36128697f93cd01
Kavith Rashintha <105831970+KavithRashintha@users.noreply.github.com>
2024-02-27 15:47:35 +0530

    Project Structure Modified

---
+++ pricing_and_discount/src/pricing_and_discount/pricing_and_discount.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PricingAndDiscountService } from './pricing_and_discount.service';
+import { PricingAndDiscountController } from './pricing_and_discount.controller';
+
+@Module({
+  providers: [PricingAndDiscountService],
+  controllers: [PricingAndDiscountController]
+})
+export class PricingAndDiscountModule {}
