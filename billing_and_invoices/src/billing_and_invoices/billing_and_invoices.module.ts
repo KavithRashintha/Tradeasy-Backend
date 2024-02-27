0c3e4a0b5038e2ddbcfc8326d125293c315286eb
Kavith Rashintha <105831970+KavithRashintha@users.noreply.github.com>
2024-02-27 15:44:54 +0530

    Project Structure Created

---
+++ billing_and_invoices/src/billing_and_invoices/billing_and_invoices.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { BillingAndInvoicesService } from './billing_and_invoices.service';
+import { BillingAndInvoicesController } from './billing_and_invoices.controller';
+
+@Module({
+  providers: [BillingAndInvoicesService],
+  controllers: [BillingAndInvoicesController]
+})
+export class BillingAndInvoicesModule {}
---
+++ item_catalog/src/item_catalog/item_catalog.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ItemCatalogService } from './item_catalog.service';
+import { ItemCatalogController } from './item_catalog.controller';
+
+@Module({
+  providers: [ItemCatalogService],
+  controllers: [ItemCatalogController]
+})
+export class ItemCatalogModule {}
