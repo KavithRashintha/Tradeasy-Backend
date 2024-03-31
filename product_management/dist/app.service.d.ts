12c09951dee7478cffb7f4c38ebaec993f15d322
Thikshana Ekanayake <110461079+Thikshana-Ekanayake@users.noreply.github.com>
2024-03-31 23:50:54 +0530

    File Structure Completed

--- product_management/dist/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from './product.entity';
-import { Repository } from 'typeorm';
-export declare class AppService {
-    private readonly productRepository;
-    constructor(productRepository: Repository<Product>);
-    getAllProducts(): Promise<Product[]>;
-}
--- product_management/dist/product.entity.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Product {
-    id: number;
-    productName: string;
-    productImage: string;
-    productQuantity: string;
-    productPrice: number;
-}
