5b67a4a69873be3f255292291a28260053ca2202
KavithRashintha <wgkrashintha@gmail.com>
2024-03-31 07:18:59 +0530

    Inventory Item's Controller file's Functionalities Added

--- inventory_management/src/app.controller.ts
+++ inventory_management/src/app.controller.ts
@@ -1,12 +1,36 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {MessagePattern, Payload} from "@nestjs/microservices";
+import {InventoryItemDTO} from "./dto/InventoryItemDTO";
+import {UpdateInventoryItemDTO} from "./dto/UpdateInventoryItemDTO";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly inventoryManagement: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern({cmd: "ADD_INVENTORY_ITEM"})
+  async addInventoryItem(@Payload() inventoryItemDto : InventoryItemDTO){
+    return await this.inventoryManagement.addInventoryItem(inventoryItemDto);
   }
+
+  @MessagePattern({cmd: "GET_INVENTORY_ITEM"})
+  async getInventoryItem(@Payload() id:any ){
+    return await this.inventoryManagement.getInventoryItem(id);
+  }
+
+  @MessagePattern({cmd: "GET_ALL_INVENTORY_ITEMS"})
+  async getAllInventoryItems(){
+    return await this.inventoryManagement.getAllInventoryItems();
+  }
+
+  @MessagePattern({cmd: "UPDATE_INVENTORY_ITEM"})
+  async updateInventoryItem(@Payload() id:any , updatedInventoryItemDto: UpdateInventoryItemDTO){
+    return await this.inventoryManagement.updateInventoryItem(id, updatedInventoryItemDto);
+  }
+
+  @MessagePattern({cmd: 'DELETE_INVENTORY_ITEM'})
+  async deleteInventoryItem(@Payload() id:any){
+    return await this.inventoryManagement.deleteInventoryItem(id);
+  }
+
 }
