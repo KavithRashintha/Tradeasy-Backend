e452f5fcc25dd4a490b48a994d4f5f60a2d1459e
Thikshana Ekanayake <110461079+Thikshana-Ekanayake@users.noreply.github.com>
2024-04-17 01:34:12 +0530

    Delete customer_management/dist directory

--- customer_management/dist/dto/CustomerDTO.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CustomerDTO {
-    readonly id: number;
-    readonly customerName: string;
-    readonly customerEmail: string;
-    readonly customerAddress: string;
-    readonly customerContact: string;
-    readonly customerPassword: string;
-}
--- customer_management/dist/dto/GetCustomerDTO.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GetCustomerDTO = void 0;
-class GetCustomerDTO {
-}
-exports.GetCustomerDTO = GetCustomerDTO;
-//# sourceMappingURL=GetCustomerDTO.js.map
