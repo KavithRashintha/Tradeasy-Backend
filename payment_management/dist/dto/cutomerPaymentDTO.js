772d521e8c82c9db9580d6627b9ae9186d9d89fd
Thikshana Ekanayake <110461079+Thikshana-Ekanayake@users.noreply.github.com>
2024-05-11 15:47:15 +0530

    Delete payment_management/dist directory

--- payment_management/dist/dto/cutomerPaymentDTO.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CustomerPaymentDTO = void 0;
-class CustomerPaymentDTO {
-}
-exports.CustomerPaymentDTO = CustomerPaymentDTO;
-//# sourceMappingURL=cutomerPaymentDTO.js.map
