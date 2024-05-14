29ba510a9ab9ea36d3375d93997ad36854716890
Jinu2001 <110693754+Jinu2001@users.noreply.github.com>
2024-05-14 10:15:49 +0530

    Add files via upload
    added inventory refund management

---
+++ inventory_refund_management/dist/app.controller.d.ts
@@ -0,0 +1,11 @@
+import { AppService } from './app.service';
+import { InventoryRefundDTO } from './dto/inventoryRedundDTO';
+import { InventoryRefund } from './inventory_refunds.entitiy';
+export declare class AppController {
+    private readonly inventoryRefundManagement;
+    constructor(inventoryRefundManagement: AppService);
+    createInventoryRefund(inventoryRefundDTO: InventoryRefundDTO): Promise<InventoryRefund>;
+    getAllInventoryRefund(): Promise<InventoryRefund[]>;
+    getInventoryRefundById(inventory_id: number): Promise<InventoryRefund>;
+    deleteCustomerRefund(inventory_id: number): Promise<"Not Deleted" | "Successfully Deleted">;
+}
