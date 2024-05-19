8468c9cdd212d76cd3ad2c055c7d2651be1bd880
Thikshana Ekanayake <110461079+Thikshana-Ekanayake@users.noreply.github.com>
2024-05-19 13:03:19 +0530

    modified product Images field in to  array type

--- product_management/src/DTO/UpdateProductDTO.ts
+++ product_management/src/DTO/UpdateProductDTO.ts
@@ -5,7 +5,7 @@ export class UpdateProductDTO {
     readonly productManufacture?: string;
     readonly productCategory?: string;
     readonly productDescription?: string;
-    readonly productImage?: string;
+    readonly productImage?: string[];
     readonly productColor?: string;
     readonly productQuantity?: number;
     readonly productSellingPrice?: number;
