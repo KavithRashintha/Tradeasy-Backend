b99cce2f7242cb9636ed464eabe0b559505813d9
Jinu2001 <110693754+Jinu2001@users.noreply.github.com>
2024-05-21 20:54:30 +0530

    Delete purchase_order_management/dist directory

--- purchase_order_management/dist/purchaseorder.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class PurchaseOrder {
-    id: number;
-    supplier: string;
-    Address: string;
-    mail: string;
-    contact_number: string;
-    items: string;
-    status: string;
-}
