ea0c53a5d70fbe254fbd07dec10d1601608b2050
Jinu2001 <110693754+Jinu2001@users.noreply.github.com>
2024-05-21 20:54:53 +0530

    Delete inventory_refund_management/dist directory

--- inventory_refund_management/dist/dto/inventoryRedundDTO.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class InventoryRefundDTO {
-    readonly supplier: string;
-    readonly item: string;
-    readonly quantity: number;
-    readonly price: number;
-    readonly reason: string;
-}
--- inventory_refund_management/dist/dto/inventoryRedundDTO.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InventoryRefundDTO = void 0;
-class InventoryRefundDTO {
-}
-exports.InventoryRefundDTO = InventoryRefundDTO;
-//# sourceMappingURL=inventoryRedundDTO.js.map
