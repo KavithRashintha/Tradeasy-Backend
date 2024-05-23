93e203be4a0162a7c265fda77715d2d1ad74d381
MrSilent2001 <110489250+MrSilent2001@users.noreply.github.com>
2024-05-23 23:59:26 +0530

    Delete purchase_order_management/dist directory

--- purchase_order_management/dist/dto/purchaseOrderDTO.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class PurchaseOrderDTO {
-    readonly supplier: string;
-    readonly Address: string;
-    readonly mail: string;
-    readonly contact_number: string;
-    readonly items: string;
-    readonly status: string;
-    readonly createdDate: Date;
-}
