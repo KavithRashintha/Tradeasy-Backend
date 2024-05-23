9346d1e22ec3ba43c0b0f8f00ca8310df688c8d6
MrSilent2001 <110489250+MrSilent2001@users.noreply.github.com>
2024-05-24 00:02:04 +0530

    Delete supplier_management/dist directory

--- supplier_management/dist/dto/SupplierDTO.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SupplierDTO = void 0;
-class SupplierDTO {
-}
-exports.SupplierDTO = SupplierDTO;
-//# sourceMappingURL=SupplierDTO.js.map
