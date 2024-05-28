c6d2421736f4caa04e0df1f7d0b63b76e296e16f
Thikshana Ekanayake <110461079+Thikshana-Ekanayake@users.noreply.github.com>
2024-05-28 10:37:43 +0530

    serviceGeneralized

--- api_gateway/src/mail.controller.ts
+++ api_gateway/src/mail.controller.ts
@@ -1,6 +1,6 @@
 import {Body, Controller, Inject, Post, UseGuards} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import {OrderStatusChangeEmailDTO} from "./models/emailModel";
+import {GeneralEmailDTO, OrderStatusChangeEmailDTO} from "./models/emailModel";
 import {JwtGuard} from './guards/jwt.guard';
 
 @Controller('email')
@@ -9,7 +9,16 @@ export class EmailController {
         @Inject('MAIL_SENDER_SERVICE') private emailClient: ClientProxy,
     ) {}
 
-    @UseGuards(JwtGuard)
+    //For Normal Emails
+    // @UseGuards(JwtGuard)
+    @Post('send')
+    async sendGeneralEmail(@Body() payload: GeneralEmailDTO) {
+        // console.log(payload);
+        return this.emailClient.send({ cmd: 'SEND_EMAIL_GENERAL' }, payload);
+    }
+
+    //For Order Status Changed Emails
+    // @UseGuards(JwtGuard)
     @Post('send/orderStatus')
     async sendOrderStatusEmail(@Body() payload: OrderStatusChangeEmailDTO) {
         // console.log(payload);
--- api_gateway/src/models/emailModel.ts
+++ api_gateway/src/models/emailModel.ts
@@ -1,3 +1,10 @@
+export class GeneralEmailDTO {
+    readonly receiverName?: string;
+    readonly emailSubject?: string;
+    readonly emailBody?: string;
+    readonly receiverEmail?: string;
+}
+
 export class OrderStatusChangeEmailDTO {
     readonly receiverName?: string;
     readonly emailSubject?: string;
